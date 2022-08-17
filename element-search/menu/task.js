6847079004cf3d3c5adc8dcb1f8565b7ab7eb9e7
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-08-17 23:15:25 +0400

    fix: fix a bug with closing the drop-down list

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,16 +1,21 @@
-const menuItem = Array.from(document.getElementsByClassName('menu__item'));
 const menuLink = Array.from(document.getElementsByClassName('menu__link'));
 
-for (let i = 0; i < menuLink.length; i++) {
-    menuLink[i].onclick = () => {
-        if (menuItem[i].querySelector('.menu_sub') && !menuItem[i].querySelector('.menu_active')) {
-            menuItem.forEach((element, index) => {
-                if (element.querySelector('.menu_active')) {
-                    menuItem[index].querySelector('.menu_sub').classList.remove('menu_active');
-                }
-            });
-            menuItem[i].querySelector('.menu_sub').classList.add('menu_active');
-                return menuLink[i] === false;
-    } 
-  };
-}
+menuLink.forEach(el => {
+    el.onclick = function () {
+        let parent = el.parentElement.querySelector('ul');
+        let active = el.closest('.menu_main').querySelector('.menu_active');
+
+        if (parent.classList.contains('menu_active')) {
+            active.classList.remove('menu_active');
+        } else {
+            if(active) {
+                active.classList.remove('menu_active');
+                parent.classList.add('menu_active')
+            } else {
+                parent.classList.add('menu_active')
+            }
+        }
+        return false;
+    };
+})
+
